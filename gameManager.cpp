b5ec1b8d5835e77c4a2d4a3fe964687bc4fc7e16
Aqibkhan2023 <63522761+Aqibkhan2023@users.noreply.github.com>
2021-04-28 01:24:03 +0500

    optimized randomization

--- gameManager.cpp
+++ gameManager.cpp
@@ -21,17 +21,18 @@ void gameManager::drawObjects() //iterating through the lists and drawing all of
 void gameManager::detectClick(int x, int y)
 {
     std::cout << "Mouse clicked at: " << x << " -- " << y << std::endl;
-    int r = rand()%6;
     for (list<Patches *>::iterator patch = patches.begin(); patch != patches.end(); ++patch)
     {
         if ((*patch)->isAvailable)
             if ((*patch)->checkRange(x, y)){
+                int r = rand()%6;
                 if (r == 0)  {towers.push_back(new FireTower(x, y)); projectiles.push_back(new Projectile(x, y));}
                 else if (r == 1) {towers.push_back(new BombTower(x, y)); projectiles.push_back(new Projectile(x, y));}
                 else if (r == 2) {towers.push_back(new GoldTower(x, y)); projectiles.push_back(new Projectile(x, y));}
                 else if (r == 3) {towers.push_back(new IceTower(x, y)); projectiles.push_back(new Projectile(x, y));}
                 else if (r == 4) {towers.push_back(new LongBowTower(x, y)); projectiles.push_back(new Projectile(x, y));}
                 else if (r == 5) {towers.push_back(new RepairTower(x, y)); projectiles.push_back(new Projectile(x, y));}
+                (*patch)->isAvailable = false;
             }
 
     }
