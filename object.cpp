417a01c2c523ba19af12422adcebe2ae27a382e2
hsntrq <mt05491@st.habib.edu.pk>
2021-04-27 09:57:32 +0500

    deploying on heroku

--- object.cpp
+++
@@ -1,5 +0,0 @@
-#include "object.hpp"
-
-Object::Object() {}                          //default constructor
-Object::Object(SDL_Rect m) : moverRect(m) {} //overloaded constructors
-Object::Object(SDL_Rect s, SDL_Rect m) : moverRect(m), srcRect(s) {}
