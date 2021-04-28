aacc53b4462196fcac7f39e17bb557660dd4f1ee
Aqibkhan2023 <63522761+Aqibkhan2023@users.noreply.github.com>
2021-04-29 00:42:10 +0500

    Code refactored - isClicked method - patches

--- gameManager.cpp
+++ gameManager.cpp
@@ -6,12 +6,12 @@ void gameManager::drawObjects() //iterating through the lists and drawing all of
     {
         (*tower)->draw(gRenderer, assets);
     }
-    for (int i=0; i<towerCards.size(); i++)
+    for (int i = 0; i < towerCards.size(); i++)
     {
         towerCards[i].draw(gRenderer, assets);
     }
     for (list<Projectile *>::iterator projectile = projectiles.begin(); projectile != projectiles.end(); ++projectile)
-    {    
+    {
         (*projectile)->draw(gRenderer, assets);
         (*projectile)->shoot(500, 416);
         if ((*projectile)->reachedTarget)
@@ -24,55 +24,47 @@ void gameManager::drawObjects() //iterating through the lists and drawing all of
 
 void gameManager::detectClick(int x, int y)
 {
-    std::cout << "Mouse clicked at: " << x << " -- " << y  << std::endl;
+    std::cout << "Mouse clicked at: " << x << " -- " << y << std::endl;
     if (cardClicked)
     {
         for (list<Patches *>::iterator patch = patches.begin(); patch != patches.end(); ++patch)
         {
-            if ((*patch)->isAvailable)
-                if ((*patch)->checkRange(x, y)){
-                    auto [a,b] = (*patch)->location();
-                    if (towerSelected == 0)  {towers.push_back(new FireTower(a, b)); projectiles.push_back(new FireTowerProjectile(a, b));}
-                    else if (towerSelected == 1) {towers.push_back(new BombTower(a, b)); projectiles.push_back(new BombTowerProjectile(a, b));}
-                    else if (towerSelected == 2) {towers.push_back(new GoldTower(a, b));}
-                    else if (towerSelected == 3) {towers.push_back(new IceTower(a, b)); projectiles.push_back(new IceTowerProjectile(a, b));}
-                    else if (towerSelected == 4) {towers.push_back(new LongBowTower(a, b)); projectiles.push_back(new LongBowTowerProjectile(a, b));}
-                    else if (towerSelected == 5) {towers.push_back(new RepairTower(a, b));}
-                    (*patch)->isAvailable = false;
-                }
+            (*patch)->isClicked(towers, projectiles, towerSelected, x, y);
         }
         cardClicked = false;
         towerCards[towerSelected].isSelected = false;
         towerSelected = -1;
     }
-    for (int i=0; i<towerCards.size(); i++)
+    else
     {
-        if (towerCards[i].isClicked(x, y))
+        for (int i = 0; i < towerCards.size(); i++)
         {
-            cardClicked = true;
-            towerSelected = i;
-            towerCards[towerSelected].isSelected = true;
-            break;
+            if (towerCards[i].isClicked(x, y))
+            {
+                cardClicked = true;
+                towerSelected = i;
+                towerCards[towerSelected].isSelected = true;
+                break;
+            }
         }
     }
-    
 }
 
-gameManager::gameManager(SDL_Renderer *renderer, SDL_Texture *asst) : gRenderer(renderer), assets(asst) {
+gameManager::gameManager(SDL_Renderer *renderer, SDL_Texture *asst) : gRenderer(renderer), assets(asst)
+{
 
-    patches.push_back(new Patches(64,480));
-    patches.push_back(new Patches(288,416));
-    patches.push_back(new Patches(288,256));
-    patches.push_back(new Patches(32,64));
-    patches.push_back(new Patches(672,160));
-    patches.push_back(new Patches(480,352));
-    patches.push_back(new Patches(544,544));
-    patches.push_back(new Patches(768,320));
-    patches.push_back(new Patches(992,512));
-    patches.push_back(new Patches(1152,416));
-    patches.push_back(new Patches(960,224));
+    patches.push_back(new Patches(64, 480));
+    patches.push_back(new Patches(288, 416));
+    patches.push_back(new Patches(288, 256));
+    patches.push_back(new Patches(32, 64));
+    patches.push_back(new Patches(672, 160));
+    patches.push_back(new Patches(480, 352));
+    patches.push_back(new Patches(544, 544));
+    patches.push_back(new Patches(768, 320));
+    patches.push_back(new Patches(992, 512));
+    patches.push_back(new Patches(1152, 416));
+    patches.push_back(new Patches(960, 224));
 
-    
     towerCards.push_back(FireCard());
     towerCards.push_back(BombCard());
     towerCards.push_back(GoldCard());
@@ -82,7 +74,6 @@ gameManager::gameManager(SDL_Renderer *renderer, SDL_Texture *asst) : gRenderer(
 
     cardClicked = false;
     towerSelected = -1;
-    
 }
 gameManager::~gameManager() //destructor deletes all dynamically created objects traversing them in all the lists
 {
--- patches.hpp
+++ patches.hpp
@@ -1,5 +1,18 @@
 #include <iostream>
 #include <tuple>
+#include <list>
+#include "object.hpp"
+#include "towers/firetower.hpp"
+#include "towers/icetower.hpp"
+#include "towers/goldtower.hpp"
+#include "towers/longbowtower.hpp"
+#include "towers/repairtower.hpp"
+#include "towers/bombtower.hpp"
+#include "projectile/projectile.hpp"
+#include "projectile/firetowerprojectile.hpp"
+#include "projectile/bombtowerprojectile.hpp"
+#include "projectile/longbowtowerprojectile.hpp"
+#include "projectile/icetowerprojectile.hpp"
 class Patches{
     private:
         int x,y;
@@ -8,4 +21,5 @@ class Patches{
         Patches(int, int);
         bool checkRange(int, int);
         std::tuple<int, int> location();
+        void isClicked(std::list<Object *> &,std::list<Projectile *> &, int, int, int);
 };
