6ea107f7df73b1f62f660205f156d2527ec90561
Aqibkhan2023 <63522761+Aqibkhan2023@users.noreply.github.com>
2021-05-04 18:25:18 +0500

    Buttons pressed functionality added

--- buttons/button.hpp
+++ buttons/button.hpp
@@ -4,11 +4,11 @@ class Button : public Object
 {
 private:
     bool isPressed;
+    SDL_Rect srcSprite[2];
 
 public:
     Button();
-    Button(SDL_Rect, SDL_Rect);
+    Button(SDL_Rect srcsprite_, SDL_Rect srcsprite_p, SDL_Rect moverRect_);
     void draw(SDL_Renderer *gRenderer, SDL_Texture *assets);
-
     bool pressed(int, int);
 };
