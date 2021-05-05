951cf40f3a167602286cffa9119cdb81a71a36f7
MuhammadUsmanSiddiqui <ms05996@st.habib.edu.pk>
2021-05-05 22:12:26 +0500

    Unnecessary function headers removed

--- towers/goldtower.hpp
+++ towers/goldtower.hpp
@@ -11,12 +11,7 @@ private:
 public:
     GoldTower();
     GoldTower(int x, int y);
-    void fireProjectile();
-    void computeHealth();
-    void repairTower();
     bool checkEnemyInRange();
-    void upgradeTower();
-    void destroyTower();
     void generateGold();
     void buildDamage();
     void buildReloadTime();
--- towers/repairtower.hpp
+++ towers/repairtower.hpp
@@ -13,12 +13,7 @@ public:
     RepairTower();
     RepairTower(int x, int y);
     void fireProjectile();
-    void computeHealth();
-    void repairTower();
     bool checkEnemyInRange();
-    void upgradeTower();
-    void destroyTower();
-    void repairTowers();
     void buildDamage();
     void buildReloadTime();
     void buildRange();
